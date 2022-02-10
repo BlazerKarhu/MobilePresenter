18b30a49d05a84f45448a0540b929ed8543ddc69
Cyroxin <Cyroxin@github.com>
2022-02-10 16:57:28 +0200

    Initial project files

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={{margin: 10}}>Open up App.js to start working! </Text>
+      <Text>You've got mail!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

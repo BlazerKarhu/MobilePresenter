91531bcb140dc659d7602422d78e4aeb296a55d3
Billy To <billyt@metropolia.fi>
2022-03-27 15:14:18 +0300

    database and local endpoints

--- server/server.js
+++ server/server.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const app = express()
+var db = require("./database.js")
+var md5 = require("md5")
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(express.static('public'))
 
@@ -10,3 +16,98 @@ app.get('/', async (req, res) => {
 app.listen(3000, () => {
     console.log(`Server listening on localhost:3000`)
   })
+
+  //GET users
+
+  app.get("/api/users", (req, res, next) => {
+    var sql = "select * from user"
+    var params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+          res.status(400).json({"error":err.message});
+          return;
+        }
+        res.json({
+            "message":"success",
+            "data":rows
+        })
+      });
+});
+
+
+// //POST an user
+
+app.post("/api/users/", (req, res, next) => {
+  var errors=[]
+  if (!req.body.password){
+      errors.push("No password specified");
+  }
+  if (!req.body.email){
+      errors.push("No email specified");
+  }
+  if (errors.length){
+      res.status(400).json({"error":errors.join(",")});
+      return;
+  }
+  var data = {
+      name: req.body.name,
+      email: req.body.email,
+      password : md5(req.body.password)
+  }
+  var sql ='INSERT INTO user (name, email, password) VALUES (?,?,?)'
+  var params =[data.name, data.email, data.password]
+  db.run(sql, params, function (err, result) {
+      if (err){
+          res.status(400).json({"error": err.message})
+          return;
+      }
+      res.json({
+          "message": "success",
+          "data": data,
+          "id" : this.lastID
+      })
+  });
+})
+
+//UPDATE an user
+
+app.patch("/api/users/:id", (req, res, next) => {
+  var data = {
+      name: req.body.name,
+      email: req.body.email,
+      password : req.body.password ? md5(req.body.password) : null
+  }
+  db.run(
+      `UPDATE user set 
+         name = COALESCE(?,name), 
+         email = COALESCE(?,email), 
+         password = COALESCE(?,password) 
+         WHERE id = ?`,
+      [data.name, data.email, data.password, req.params.id],
+      function (err, result) {
+          if (err){
+              res.status(400).json({"error": res.message})
+              return;
+          }
+          res.json({
+              message: "success",
+              data: data,
+              changes: this.changes
+          })
+  });
+})
+
+//DELETE an user
+
+app.delete("/api/users/:id", (req, res, next) => {
+  db.run(
+      'DELETE FROM user WHERE id = ?',
+      req.params.id,
+      function (err, result) {
+          if (err){
+              res.status(400).json({"error": res.message})
+              return;
+          }
+          res.json({"message":"deleted", changes: this.changes})
+  });
+})

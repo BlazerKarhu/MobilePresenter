7525ba1f6c5e751e0143f41f568c136b3576785d
Cyroxin <Cyroxin@github.com>
2022-04-01 00:27:47 +0300

    [Client] Cleanup imports

--- client/scripts/utils/fetch.js
+++ client/scripts/utils/fetch.js
@@ -1,11 +1,11 @@
-import auth, { lastPassword } from '../database/auth'
+import auth from '../database/auth'
 
 const doFetch = async (url, options = {}) => {
   const response = await fetch(url, options);
   const json = await response.json();
   if (json.error) {
     if (response.status == 401 && auth.lastUsername != undefined && auth.lastPassword != undefined) {
-      
+
       // Unauthorized, most likely due to token expiration, try again after logging in again
       if (await auth.login(auth.lastUsername, auth.lastPassword))
         return await doFetch(url, { ...options, 'Authorization': auth.token })
